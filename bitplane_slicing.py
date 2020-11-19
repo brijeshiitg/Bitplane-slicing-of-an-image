5be95e55cb09bd03f043bbe02a7492a209530119
Brijesh Singh <brijesh.singh@iitg.ac.in>
2020-11-19 23:20:42 +0530

    Update bitplane_slicing.py

--- bitplane_slicing.py
+++ bitplane_slicing.py
@@ -8,7 +8,7 @@ def int2bitarray(img):
             arr.append(np.binary_repr(img[i][j], width=8))
     return arr
 # read image convert to bit stream
-img = cv2.imread('/Users/administrator/Desktop/My_thesis/chapters/Chapter_6/Lena/lena.png',0)
+img = cv2.imread('./lena.png',0)
 arr = np.array(int2bitarray(img))
 arr = arr.reshape(img.shape)
 
@@ -17,5 +17,5 @@ for k in range(0,8):
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
             plane[i,j]=int(arr[i,j][k])
-    cv2.imwrite('/Users/administrator/Desktop/Bitplane'+str(7-k)+'.png',plane*255)
+    cv2.imwrite('./Bitplane'+str(7-k)+'.png',plane*255)
     print('\nbit plane '+str(7-k)+' done!')
